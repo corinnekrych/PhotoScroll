be8ca8592f49eee6a173d18ce370891f5902e8d6
corinne krych <corinnekrych@gmail.com>
2015-11-21 18:46:28 +0100

    Inital project

---
+++ PhotoScroll/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  PhotoScroll
+//
+//  Created by Corinne Krych on 21/11/15.
+//  Copyright © 2015 raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    private let reuseIdentifier = "PhotoCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let photos = ["", "", ""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+// MARK: Implementation of UICollectionViewDataSource
+extension ViewController {
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) 
+        cell.backgroundColor = UIColor.blackColor()
+        // Configure the cell
+        return cell
+    }
+}
+
+// MARK: Implementation of UICollectionViewDelegateFlowLayout
+extension ViewController : UICollectionViewDelegateFlowLayout {
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return sectionInsets
+    }
+}
