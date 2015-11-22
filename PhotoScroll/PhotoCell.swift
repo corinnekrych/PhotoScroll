9ab1460643d02fea3a49139d8a9e2b41b725d128
corinne krych <corinnekrych@gmail.com>
2015-11-22 21:29:55 +0100

    initial project

---
+++ PhotoScroll/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  PhotoScroll
+//
+//  Created by Corinne Krych on 21/11/15.
+//  Copyright © 2015 raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell : UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+}
--- PhotoScroll/ViewController.swift
+++ PhotoScroll/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UICollectionViewController {
     private let reuseIdentifier = "PhotoCell"
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    private let photos = ["", "", ""]
+    private let photos = ["photo1", "photo2", "photo3", "photo4", "photo5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
