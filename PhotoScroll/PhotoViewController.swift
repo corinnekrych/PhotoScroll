8ac27275797e6d7aa8942c18c2c24e2d086d1236
corinne krych <corinnekrych@gmail.com>
2015-11-22 23:37:26 +0100

    zommed and centered

--- PhotoScroll/PhotoViewController.swift
+++ PhotoScroll/PhotoViewController.swift
@@ -15,9 +15,41 @@ class PhotoViewController: UIViewController {
     
     override func viewDidLoad() {
         imageView.image = UIImage(named: photoName)
-        scrollView.minimumZoomScale = 0.1
+        //scrollView.minimumZoomScale = 0.1
+        //scrollView.maximumZoomScale = 3.0
+        //scrollView.zoomScale = 1.0
+    }
+    
+    func setZoomParametersForSize(scrollViewSize: CGSize) {
+        let imageSize = imageView.bounds.size
+        
+        let widthScale = scrollViewSize.width / imageSize.width
+        let heightScale = scrollViewSize.height / imageSize.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
         scrollView.maximumZoomScale = 3.0
-        scrollView.zoomScale = 1.0
+        scrollView.zoomScale = minScale
+    }
+    
+    override func viewWillLayoutSubviews() {
+        setZoomParametersForSize(scrollView.bounds.size)
+        recenterImage()
+    }
+    
+    func recenterImage() {
+        let scrollViewSize = scrollView.bounds.size
+        let imageSize = imageView.frame.size
+        
+        let horizontalSpace = imageSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageSize.width) / 2 : 0
+        let verticalSpace = imageSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - CGRectGetHeight(navigationController!.navigationBar.frame) - imageSize.height) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalSpace,
+            left: horizontalSpace,
+            bottom: verticalSpace,
+            right: horizontalSpace)
     }
 }
 
